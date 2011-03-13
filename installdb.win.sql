0de289f48ac3daaf367e48c98895ecd256e8a4c5
Daniel Trebbien <dtrebbien@gmail.com>
2011-03-13 11:49:16 -0700

    Adapt the installdb script for Linux and Windows
    On Linux with MySQL 5.1, the '.so' extension is apparently needed.

---
+++ installdb.win.sql
@@ -0,0 +1,21 @@
+use mysql;
+
+drop function if exists lib_mysqludf_str_info;
+drop function if exists str_numtowords;
+drop function if exists str_rot13;
+drop function if exists str_shuffle;
+drop function if exists str_translate;
+drop function if exists str_ucfirst;
+drop function if exists str_ucwords;
+drop function if exists str_xor;
+drop function if exists str_srand;
+
+create function lib_mysqludf_str_info returns string soname 'lib_mysqludf_str.dll';
+create function str_numtowords returns string soname 'lib_mysqludf_str.dll';
+create function str_rot13 returns string soname 'lib_mysqludf_str.dll';
+create function str_shuffle returns string soname 'lib_mysqludf_str.dll';
+create function str_translate returns string soname 'lib_mysqludf_str.dll';
+create function str_ucfirst returns string soname 'lib_mysqludf_str.dll';
+create function str_ucwords returns string soname 'lib_mysqludf_str.dll';
+create function str_xor returns string soname 'lib_mysqludf_str.dll';
+create function str_srand returns string soname 'lib_mysqludf_str.dll';
